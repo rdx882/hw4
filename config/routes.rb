e40e0845597516e8b0e0d909d8d78cc5ec91e9cd
Roland Deex <roland.deex@kellogg.northwestern.edu>
2025-03-16 16:32:11 +0000

    fix of file upload

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+  get("/login", { :controller => "sessions", :action => "new" })
+  get("/signup", { :controller => "sessions", :action => "new" })
+  get("/posts", { :controller => "places", :action => "index" })
+  get("/logout", { :controller => "sessions", :action => "destroy" })
 end

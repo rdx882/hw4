9fac72ba47cddd4a1f40d8e8a608689820cb13be
Roland Deex <roland.deex@kellogg.northwestern.edu>
2025-03-16 16:10:07 +0000

    controllers setup

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,14 +1,18 @@
 class EntriesController < ApplicationController
 
   def new
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
+    @user = User.find_by({ "id" => session["user_id"] })
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
+    @entry["user_id"] = @user["id"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,10 +6,12 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def new
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
